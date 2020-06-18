54bb378f16b413d7c9cd1738cb1542d682b0dc6f
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-06-18 10:21:45 +0200

    v.0.1.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function difference() {
     var x = document.getElementById('age1')
     var y = document.getElementById('age2')
-    var z = x - y;
+    var z = x.value - y.value;
     document.getElementById('result').innerHTML = ("Die Differenz ist: " + z )
 }
